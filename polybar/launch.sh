3328fcf7ecd5554c9bda5a0bff48df16fc88ee53
Hayden Roszell <hroszell@gmail.com>
2024-04-03 23:15:57 -0700

    chore(linux): Linux stuff

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload thebar &
+  done
+else
+  polybar --reload thebar &
+fi
