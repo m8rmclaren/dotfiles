39b8104593000f7751162f04cc3db85abb678af6
Hayden Roszell <hroszell@gmail.com>
2024-04-21 23:28:44 -0700

    chore(aliases): change to local path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+
 ln -s $HOME/.config/scripts/sessionizer ~/.local/bin
 
 # Install tmux plugin manager
@@ -6,3 +11,8 @@ git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 # Install zsh-syntax-highlighting
 apt install zsh-syntax-highlighting
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+# Put in .bashrc or .zshrc
+source $HOME/.config/zsh/aliases.sh
+export PATH="$PATH:/opt/nvim-linux64/bin"
+export PATH="$HOME/.local/bin:$PATH"
--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,7 +1,7 @@
 source <(kubectl completion zsh)
 
 alias projectizer='source $HOME/.config/scripts/projectizer'
-alias cdd='source /Users/hroszell/.config/scripts/cdd'
-alias aliaser='source /Users/hroszell/.config/scripts/aliaser'
-alias gotest='source /Users/hroszell/.config/scripts/go/gotest.sh'
+alias cdd='source $HOME/.config/scripts/cdd'
+alias aliaser='source $HOME/.config/scripts/aliaser'
+alias gotest='source $HOME/.config/scripts/go/gotest.sh'
 alias edit='nvim .'
