48c1f20e1517ba20f700f060599dc51e4d04b455
Hayden Roszell <hroszell@gmail.com>
2024-09-18 16:01:06 -0700

    chore(harpoon): Rebuild Telescope integration for Harpoon
    Signed-off-by: Hayden Roszell <hroszell@gmail.com>

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,17 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').load_extension('harpoon')
+
+require('telescope').setup {
+    extensions = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
+        --   extension_config_key = value,
+        -- }
+        -- please take a look at the readme of the extension you want to configure
+    }
+}
+
 -- Regular pickers
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Fuzzy find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Fuzzy find in files' })
@@ -16,7 +28,8 @@ vim.keymap.set('n', '<leader>fjl', builtin.jumplist, { desc = 'Fuzzy find jump l
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'Go to definition' })
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Fuzzy find references to the value under cursor' })
 vim.keymap.set('n', '<leader>fqf', builtin.quickfix, { desc = 'Fuzzy find quickfix list' })
-vim.keymap.set('n', '<leader>ft', builtin.lsp_type_definitions, { desc = 'Fuzzy find type definitions of the variable under cursor' })
+vim.keymap.set('n', '<leader>ft', builtin.lsp_type_definitions,
+    { desc = 'Fuzzy find type definitions of the variable under cursor' })
 
 -- Treesitter pickers
 -- vim.keymap.set('n', '<leader>ft', builtin.treesitter, { desc = 'Fuzzy find treesitter for the current buffer' })
